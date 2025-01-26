b06544fb44507d471cea5de036894e3ccd75808a
Abdo <abdo180294@gmail.com>
2025-01-25 22:56:15 -0600

    add about me and skills section and fix bugs

--- script.js
+++ script.js
@@ -1,8 +1,23 @@
 const navMenu = document.querySelector('.nav-menu')
 const hamSvg = document.querySelector('.ham');
 const hamButton = document.querySelector('.ham-button');
+const menuItems = document.querySelectorAll('.nav-menu a');
 
 hamButton.addEventListener('click', () => {
   navMenu.classList.toggle('active');
   hamSvg.classList.toggle('active');
 })
+
+menuItems.forEach(item => {
+  navMenu.style.pointerEvents = "none"; 
+
+  setTimeout(() => {
+    navMenu.style.pointerEvents = "auto"; 
+  }, 100);
+  
+  item.addEventListener('click', () => {
+    navMenu.classList.toggle('active');
+    hamSvg.classList.toggle('active');
+  })
+})
+
